f33c65115defbcdbf2726e65a04e19117b9f2d1d
Mehret Kulala <merutaye@gmail.com>
2018-04-30 21:26:51 -0500

    filter-fish working

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,25 @@
 // Filter fish that are "on sale"
+const filterFish = () => {
+  $('#available .fish').not('.on-sale').toggle();
+};
+
+const changeButtonText = () => {
+  $('#show-sale').text((i, text) => {
+    let returnText = '';
+     if (text === "Show Sale Fish") {
+       returnText = "Show All"
+     } else {
+       returnText = "Show Sale Fish"
+     }
+     return returnText;
+  })
+}
+
+$('#show-sale').click(() => {
+  changeButtonText();
+  filterFish();
+});
+
 
 // Add fish to "Basket"
 const moveToCart = (e) => {
@@ -7,4 +28,4 @@ const moveToCart = (e) => {
   $('#snagged').append(fishCard);
 };
 
-$('button.add').click(moveToCart);
+$('button.add').click(moveToCart);;
