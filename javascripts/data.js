17f10d0afffb52c47906eb6c62410f54250e7711
Mehret Kulala <merutaye@gmail.com>
2018-05-01 19:40:08 -0500

    printing todom works

---
+++ javascripts/data.js
@@ -0,0 +1,18 @@
+const loadFishes = require('./fishes');
+const writeFishes = require('./fishDom');
+
+const whenFishesLoad = (data) => {
+  console.log('data', data);
+  $('#available').append(writeFishes(data.fishes)); // selecting the div element and appending the writeFishfunction
+
+};
+
+const whenFishesDontLoad = (error) => {
+  console.error('error!', error);
+};
+
+const initializer = () => {
+  loadFishes(whenFishesLoad, whenFishesDontLoad);
+};
+
+module.exports = initializer;
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,6 @@
 console.log('hi');
-const bindEvents = require ('./events');
+const bindEvents = require('./events');
+const initializer = require('./data');
 
+initializer();
 bindEvents();
