ffb71fc24a061ea9f5e3cfcce0c6f53b4efe99a0
Mehret Kulala <merutaye@gmail.com>
2018-05-01 19:49:01 -0500

    events working

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,10 +1,11 @@
 const loadFishes = require('./fishes');
 const writeFishes = require('./fishDom');
+const bindEvents = require('./events');
 
 const whenFishesLoad = (data) => {
   console.log('data', data);
   $('#available').append(writeFishes(data.fishes)); // selecting the div element and appending the writeFishfunction
-
+  bindEvents();
 };
 
 const whenFishesDontLoad = (error) => {
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,4 @@
 console.log('hi');
-const bindEvents = require('./events');
 const initializer = require('./data');
 
 initializer();
-bindEvents();
